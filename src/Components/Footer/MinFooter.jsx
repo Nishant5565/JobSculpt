5f5c57e57d0c81944687a3ebf0105722720da274
Nishant <ntih5565@gmail.com>
2024-10-07 22:24:18 +0530

    Whole website changed

--- src/Components/Footer/MinFooter.jsx
+++ src/Components/Footer/MinFooter.jsx
@@ -4,7 +4,7 @@ import { ThemeContext } from '../../Pages/ThemeContext';
 const MinFooter = () => {
   const { theme } = useContext(ThemeContext);
   return (
-    <footer className={`py-10 text-center flex flex-col justify-between h-40 md:flex-row md:justify-center gap-2 md:items-center  text-[12px] mx-10 rounded-[20px] mb-10 text-white bg-black ${theme == 'dark' ?"border-2 border-gray-200 ":""} `}>
+    <footer className={`py-10 mt-6 text-center flex flex-col justify-between h-40 md:flex-row md:justify-center gap-2 md:items-center  text-[12px] mx-10 rounded-[20px] mb-10 text-white bg-black ${theme == 'dark' ?"border-2 border-gray-200 ":""} `}>
       
         <p>© 2023 - 2024</p>
         <p>JobSculpt® Global Inc.</p>
